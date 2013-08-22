08f4b1eaf7dcb1bdbd651fe462b2dfda58785e9d
alfonso <alfonso.cueto@hotmail.es>
2013-08-22 11:09:26 +0200

    añado clase de utilidad Transforms para trabajar con transforms. Añado en esta metodo para obtener un transform hijo dado un nombre y el padre

---
+++ Assets/GUIMultiresolucion/Utiles/Transforms.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class Transforms : MonoBehaviour {
+	
+	/// <summary>
+	/// Encuentra un Transform hijo, dado un nombre y el transform padre
+	/// </summary>
+	/// <returns>
+	/// El transform.
+	/// </returns>
+	/// <param name='parent'>
+	/// El transform padre.
+	/// </param>
+	/// <param name='name'>
+	/// Nombre del hijo a encontrar
+	/// </param>
+	public static Transform FindTransform(Transform parent, string name){
+        if (parent.name.Equals(name)) return parent;
+
+        foreach (Transform child in parent){
+            Transform result = FindTransform(child, name);
+
+            if (result != null) return result;
+        }
+
+        return null;
+
+    }
+}
